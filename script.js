7a7b65bfb55f826506afb72b5409db65ced1e73f
nam-web <65065658+nam-web@users.noreply.github.com>
2023-12-24 15:20:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let counterValue = 0;
+function increment(){
+	alert(counterValue);
+	counterValue++;
+	document.getElementById('counter').textContent = counterValue;
+}
